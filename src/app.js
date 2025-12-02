b51ffa6d2a639ce651f99cda13b0d36b2cff2b59
Diego Osuna <dieouna@gmail.com>
2025-12-02 18:50:34 -0500

    tue6 50pm

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
@@ -10,19 +9,34 @@ window.onload = function () {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
-  let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+  const pictures = [
+    "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1627720/capsule_616x353.jpg?t=1764565054",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhDiB5RB_xLJ0219yyRHoB7KQslkKbyi2-EQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu6lM8sP8YAP4WLyervrY7vdvSWPvvduF0LQ&s",
+  ];
   const excuseLine = document.querySelector("#excuse");
+  const picture = document.querySelector("#pictures");
+
   function getrandomindex(array) {
     const randomindex = Math.floor(Math.random() * array.length);
     return randomindex;
   }
+  
   let whoindex = getrandomindex(who);
   let actionindex = getrandomindex(action);
   let whatindex = getrandomindex(what);
   let whenindex = getrandomindex(when);
+
   excuseLine.innerHTML = `${who[whoindex]} ${action[actionindex]} ${what[whatindex]} ${when[whenindex]}`;
   
 
-
+  picture.src = pictures[getrandomindex(pictures)];
   console.log(`${what} ${who} ${when} ${action}`);
 };
